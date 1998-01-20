ae8fe3d038bb3a8538149c8f1b5c3bf3782a3ffb
stlancas <stlancas@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1998-01-20 20:33:04 +0000

    added grain diameter stuff and chan/hydr geom stuff; CalcMigration().

--- Child/Code/tStreamMeander/tStreamMeander.h
+++ Child/Code/tStreamMeander/tStreamMeander.h
@@ -7,20 +7,8 @@
 **  Contains functions for constructing reaches and calculating lateral
 **  channel migration for nodes in those reaches.
 **
-how about this: tStreamMeander contains a pointer to a tStreamNet object;
-it has a constructor that takes a ref. to a tGrid object as an argument;
-this constructor automatically creates a tStreamNet object if the pointer
-is NULL; grid data will then be accessed through the grid ptr in tStreamNet;
-calling this constructor guarantees that the necessary data items calc'ed
-by tStreamNet are indeed done. tStreamNet can still stand alone, but you can't
-meander without a tStreamNet. Could do something similar for erosion?
-Since the tStreamNet doesn't contain its own data to speak of, it wouldn't
-matter too much if more than one net were constructed. On the other hand, also 
-have a meander constructor which takes a reference to a net object;
-could also have a gridPtr member of meander, but that would need to assume
-that the net stuff had been done...
 **
-**  $Id: tStreamMeander.h,v 1.3 1998-01-17 23:29:23 stlancas Exp $
+**  $Id: tStreamMeander.h,v 1.4 1998-01-20 20:33:04 stlancas Exp $
 \**************************************************************************/
 #ifndef TSTREAMMEANDER_H
 #define TSTREAMMEANDER_H
@@ -38,27 +26,34 @@ public:
    void setReachList( const tList< tReach > & );
    void FindMeander();
    void MakeReaches();
-   void InterpChannel();
    void FindHydrGeom();
+   void FindChanGeom();
+   void InterpChannel();
    void MakeChanBorder();
-   void FindBankErody();
+   tArray< float > FindBankErody( tLNode * );
      //CheckBanksTooClose, CheckFlowedgCross, and CheckBrokenFlowedg are to check
      //for 'violations' peculiar to moving channels.
    int CheckBanksTooClose();
    int CheckFlowedgCross();
    int CheckBrokenFlowedg();
-   void CalcMigration();
+   void CalcMigration( tStorm & ); //calls meander_; pass it a ref to a storm
    void Migrate();
    void AddChanBorder();
 
 protected:
+   //ptrs and ptr lists
    tGrid< tLNode > *gridPtr;
    tStreamNet *netPtr;
    //tList< tReach > reachList;
    tList< tPtrList< tLNode > > reachList;
    //tListIter< tReach > rlIter;
    tListIter< tPtrList< tLNode > > rlIter;
+   //data items/parameters
    float critflow;
+   int optdiamvar;
+   float meddiam;
+   float kwds, ewds, ewstn;//coeff's & exp's for dwnstrm & at-a-stn hydr. width
+   float knds, ends, enstn;//coeff's & exp's for dwnstrm & at-a-stn hydr. roughness
    tArray< int > nrnodes;
    tArray< float > reachlen;
    tArray< float > taillen;
