004bb70d1d709ff109439126f520f3631cb40dd5
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1998-01-21 22:10:20 +0000

    cleanup #includes

--- Child/Code/tStorm/tStorm.cpp
+++ Child/Code/tStorm/tStorm.cpp
@@ -19,22 +19,13 @@
 **  is included in this file.
 **
 **  Version 1.0, Greg Tucker, November 1997.
-**  $Id: tStorm.cpp,v 1.1 1998-01-14 20:46:05 gtucker Exp $
+**  $Id: tStorm.cpp,v 1.2 1998-01-21 22:10:20 gtucker Exp $
 */
-#include <iostream.h>
-#include <fstream.h>
-#include <assert.h>
+
 #include <math.h>
-
-#include "../Definitions.h"
-#include "../Classes.h"
-#include "../GlobalFns.h"
-#include "../tInputFile/tInputFile.h"
-#include "../tRunTimer/tRunTimer.h"
-
+#include "../Mathutil/mathutil.h"
 #include "tStorm.h"
 
-//double ran3( long * );  // Random number generator from Numerical Recipes in C
 
 
 /*
@@ -53,6 +44,8 @@ tStorm::tStorm( int optvar )
    istdur = 1.0;
    srand( 0 );
 }
+
+
 /*
 **  tStorm::tStorm:  The constructor that's really used assigns values for
 **                   mean storm depth, duration, and interstorm duration,
@@ -214,57 +207,4 @@ double tStorm::GammaDev(double m, long * idum)
     }
   return x;
 }
-
-
-/*
-**  ran3:  Random number generator from Numerical Recipes.
-*/
-#define MBIG 1000000000
-#define MSEED 161803398
-#define MZ 0
-#define FAC (1.0/MBIG)
-
-double ran3(long *idum)
-{
-        static int inext,inextp;
-        static long ma[56];
-        static int iff=0;
-        long mj,mk;
-        int i,ii,k;
-
-        if (*idum < 0 || iff == 0) {
-                iff=1;
-                mj=MSEED-(*idum < 0 ? -*idum : *idum);
-                mj %= MBIG;
-                ma[55]=mj;
-                mk=1;
-                for (i=1;i<=54;i++) {
-                        ii=(21*i) % 55;
-                        ma[ii]=mk;
-                        mk=mj-mk;
-                        if (mk < MZ) mk += MBIG;
-                        mj=ma[ii];
-                }
-                for (k=1;k<=4;k++)
-                        for (i=1;i<=55;i++) {
-                                ma[i] -= ma[1+(i+30) % 55];
-                                if (ma[i] < MZ) ma[i] += MBIG;
-                        }
-                inext=0;
-                inextp=31;
-                *idum=1;
-        }
-        if (++inext == 56) inext=1;
-        if (++inextp == 56) inextp=1;
-        mj=ma[inext]-ma[inextp];
-        if (mj < MZ) mj += MBIG;
-        ma[inext]=mj;
-        return mj*FAC;
-}
-#undef MBIG
-#undef MSEED
-#undef MZ
-#undef FAC
-
-
 
