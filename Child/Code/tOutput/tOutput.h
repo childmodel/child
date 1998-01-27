2591df2fa1d958ce5f4b4c2fd957cd802af302a3
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1998-01-27 23:41:39 +0000

    defined tLOutput ("landscape output") derived class

--- Child/Code/tOutput/tOutput.h
+++ Child/Code/tOutput/tOutput.h
@@ -4,35 +4,45 @@
 **
 **  NB: inherit from this basic class to do output for tLNode objects.
 **
-**  $Id: tOutput.h,v 1.1 1998-01-21 01:25:34 gtucker Exp $
+**  $Id: tOutput.h,v 1.2 1998-01-27 23:41:39 gtucker Exp $
 \*************************************************************************/
 
 #ifndef TOUTPUT_H
 #define TOUTPUT_H
 
 #include <iostream.h>
-#include "../GridElements/gridElements.h"
 #include "../tInputFile/tInputFile.h"
-#include "../tListInputData/tListInputData.h"
 #include "../tGrid/tGrid.h"
-
 
 #define kMaxNameSize 80
 
+template< class tSubNode >
 class tOutput
 {
 public:
-    tOutput( tGrid<tNode> * gridPtr, tInputFile &infile );
+    tOutput( tGrid<tSubNode> * gridPtr, tInputFile &infile );
     void WriteOutput( float time );
     virtual void WriteNodeData( float time );
 
-private:
-    tGrid<tNode> * g;
+protected:
+    tGrid<tSubNode> * g;
     char baseName[kMaxNameSize];
     ofstream nodeofs;
     ofstream edgofs;
     ofstream triofs;
     ofstream zofs;
+};
+
+template< class tSubNode >
+class tLOutput : public tOutput<tSubNode>
+{
+public:
+    tLOutput( tGrid<tSubNode> * gridPtr, tInputFile &infile );
+    void WriteNodeData( float time );
+private:
+    ofstream drareaofs;  // Drainage areas
+    ofstream netofs;     // Downstream neighbor IDs
+    
 };
 
 #endif
