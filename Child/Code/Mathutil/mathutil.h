c3bd4beecd36ddab62c4af2312773be0054dfb6e
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1998-07-15 14:39:03 +0000

    Initial revision

---
+++ Child/Code/Mathutil/mathutil.h
@@ -0,0 +1,14 @@
+/*
+**  mathutil.h:  Header file for special math utilities not in math.h.
+**               All or most routines from Numerical Recipes in C by
+**               Press et al.
+**
+**  $Id: mathutil.h,v 1.1 1998-07-15 14:39:03 gtucker Exp $
+*/
+
+#include <math.h>
+
+#define PI 3.1415926
+
+double ran3( long *idum );
+
