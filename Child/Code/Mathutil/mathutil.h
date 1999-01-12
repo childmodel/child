4be775543cf6e65b4edbdcaa883f393f04108bdd
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1999-01-12 20:57:57 +0000

    *** empty log message ***

--- Child/Code/Mathutil/mathutil.h
+++ Child/Code/Mathutil/mathutil.h
@@ -1,10 +1,10 @@
-/*
+/*********************************************************************\
 **  mathutil.h:  Header file for special math utilities not in math.h.
 **               All or most routines from Numerical Recipes in C by
 **               Press et al.
 **
-**  $Id: mathutil.h,v 1.2 1999-01-05 21:21:14 gtucker Exp $
-*/
+**  $Id: mathutil.h,v 1.3 1999-01-12 20:57:57 gtucker Exp $
+\*********************************************************************/
 
 #ifndef MATHUTIL_H
 #define MATHUTIL_H
