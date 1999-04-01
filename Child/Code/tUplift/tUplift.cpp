39d433a4d586f3161916f66c536de2c74adfadb6
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
1999-04-01 19:09:32 +0000

    inline doc

--- Child/Code/tUplift/tUplift.cpp
+++ Child/Code/tUplift/tUplift.cpp
@@ -1,16 +1,30 @@
 /************************************************************************\
 **
-**  tUplift.cpp: Functions for class tUplift.
+**  tUplift.cpp: Functions for class tUplift (see tUplift.h).
 **
-**  $Id: tUplift.cpp,v 1.4 1998-06-04 21:26:54 gtucker Exp $
+**  $Id: tUplift.cpp,v 1.5 1999-04-01 19:09:32 gtucker Exp $
 \************************************************************************/
 
 #include "tUplift.h"
 #include "../errors/errors.h"
 
-#define kNumUpliftTypes 1
+#define kNumUpliftTypes 2
 #define kNoUplift 0
 
+
+/************************************************************************\
+**
+**  tUplift constructor
+**
+**  Reads in a type code from the input file, then reads parameters
+**  needed for the specified type of uplift.
+**
+**  The current version supports two types of uplift: spatially uniform,
+**  and uniform where Y>=Yf (Yf=fault location), zero elsewhere.
+**
+**  Inputs:  infile -- input file from which to read parameters
+**
+\************************************************************************/
 tUplift::tUplift( tInputFile &infile )
 {
    // Find out what kind of uplift the user wants
@@ -21,7 +35,8 @@ tUplift::tUplift( tInputFile &infile )
            << typeCode << ")\n";
       cerr << "Valid uplift types are:\n";
       cerr << " 0 - none\n"
-           << " 1 - Spatially and temporally uniform uplift\n";
+           << " 1 - Spatially and temporally uniform uplift\n"
+           << " 2 - Uniform uplift at Y >= fault location, zero elsewhere\n";
       ReportFatalError( "Please specify a valid uplift type and try again." );
    }
 
@@ -32,11 +47,25 @@ tUplift::tUplift( tInputFile &infile )
    rate = infile.ReadItem( rate, "UPRATE" );
    switch( typeCode )
    {
+   case 2:
+     faultPosition = infile.ReadItem( faultPosition, "FAULTPOS" );
+     break;
    }
    
 }
 
 
+/************************************************************************\
+**
+**  tUplift::DoUplift
+**
+**  Calls the appropriate function to perform the desired type of
+**  uplift.
+**
+**  Inputs:  gp -- pointer to the mesh
+**           delt -- duration of uplift
+**
+\************************************************************************/
 void tUplift::DoUplift( tGrid<tLNode> *gp, double delt )
 {
    switch( typeCode )
@@ -44,11 +73,25 @@ void tUplift::DoUplift( tGrid<tLNode> *gp, double delt )
       case 1:
           UpliftUniform( gp, delt );
           break;
+      case 2:
+          BlockUplift( gp, delt );
+          break;
    }
    
 }
 
 
+/************************************************************************\
+**
+**  tUplift::UpliftUniform
+**
+**  Uniform uplift at a constant rate across the entire domain (but not
+**  including boundaries).
+**
+**  Inputs:  gp -- pointer to the mesh
+**           delt -- duration of uplift
+**
+\************************************************************************/
 void tUplift::UpliftUniform( tGrid<tLNode> *gp, double delt )
 {
    assert( gp>0 );
@@ -64,6 +107,38 @@ void tUplift::UpliftUniform( tGrid<tLNode> *gp, double delt )
 }
 
 
+/************************************************************************\
+**
+**  tUplift::BlockUplift
+**
+**  Uplift at a constant rate for all points Y>=Yf, where Yf is the
+**  location of a vertical fault (striking parallel to x-axis).
+**
+**  Inputs:  gp -- pointer to the mesh
+**           delt -- duration of uplift
+**
+\************************************************************************/
+void tUplift::BlockUplift( tGrid<tLNode> *gp, double delt )
+{
+   assert( gp>0 );
+   tLNode *cn;
+   tGridListIter<tLNode> ni( gp->getNodeList() );
+   double rise = rate*delt;
+
+   for( cn=ni.FirstP(); ni.IsActive(); cn=ni.NextP() )
+     if( cn->getY()>=faultPosition )
+     {
+	cn->ChangeZ( rise );
+	cn->setUplift( rate );
+     }
+}
+
+
+/************************************************************************\
+**
+**  tUplift "get" functions
+**
+\************************************************************************/
 
 double tUplift::getDuration() 
 {
