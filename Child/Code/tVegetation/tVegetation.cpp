36bcdd7f8cc6cccc1fa2e15c63873ca8fe2da66b
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2000-01-25 18:58:28 +0000

    Initial revision

---
+++ Child/Code/tVegetation/tVegetation.cpp
@@ -0,0 +1,134 @@
+/**************************************************************************\
+**
+**  tVegetation.cpp: Functions for tVegetation and tVegCover classes
+**
+**  Classes tVegetation and tVegCover represents the vegetation cover
+**  across a terrain. Class tVegCover represents the properties of
+**  vegetation at a point on the landscape (e.g., the %cover), while
+**  class tVegetation represents the landscape-wide ("global")
+**  properties (e.g., regrowth coefficient). Class tVegCover is designed
+**  to be embedded in a node (in other words, each node "has a"
+**  vegetation cover).
+**
+**  Created January, 2000, GT
+**  
+**  $Id: tVegetation.cpp,v 1.1 2000-01-25 18:58:17 gtucker Exp $
+\**************************************************************************/
+
+#include <assert.h>
+#include "tVegetation.h"
+#include "../tMesh/tMesh.h"
+#include "../tMeshList/tMeshList.h"
+
+/**************************************************************************\
+**                 FUNCTIONS FOR CLASS tVegetation
+\**************************************************************************/
+
+/**************************************************************************\
+**
+**  tVegetation constructors:
+**    1. Default
+**    2. Input file
+**
+\**************************************************************************/
+
+tVegetation::tVegetation()
+{
+   mdKvd = mdTauCritBare = mdTauCritVeg = 0;
+   mdTVeg = 1;
+}
+
+tVegetation::tVegetation( tMesh<tLNode> * meshPtr, tInputFile &infile )
+{
+   mdKvd = infile.ReadItem( mdKvd, "VEG_KVD" );
+   mdTVeg = infile.ReadItem( mdTVeg, "VEG_TV" );
+   mdTauCritBare = infile.ReadItem( mdTauCritBare, "TAUC" );
+   mdTauCritVeg = infile.ReadItem( mdTVeg, "VEG_TAUCVEG" );
+
+   // Loop through nodes and set initial vegetation cover & threshold
+   // (for now, assume constant; later need to add restart capability)
+   // Note: assumes initially 100% cover.
+   tMeshListIter<tLNode> niter( meshPtr->getNodeList() );
+   tLNode * cn;
+   for( cn=niter.FirstP(); niter.IsActive(); cn=niter.NextP() )
+   {
+       cn->getVegCover().mdVeg = 1.0;
+       cn->setTauCrit( mdTauCritBare + mdTauCritVeg );
+   }
+   
+}
+
+/**************************************************************************\
+**
+**  tVegetation::UpdateVegetation
+**
+**  This routine updates the % vegetation cover at each node in response
+**  to a storm event and its ensuing interstorm period. It also updates
+**  the critical shear stress at each node according to its present
+**  vegetation cover.
+**
+**  Erosion of vegetation during a storm is computed as:
+**    dV/dt = -Kvd V ( tau - tauc )
+**  Where V represents the proportional cover (0 to 1), tau is shear
+**  stress, tauc is critical shear stress, and Kvd is a vegetation
+**  erodibility coefficient. The equation is solved using an analytical
+**  solution, using the expedient approximation that tauc is constant
+**  during a given storm.
+**
+**  Regrowth of vegetation following a storm is computed as:
+**    dV/dt = 1/Tv ( 1 - V )
+**  where Tv is the timescale of vegetation regrowth. An analytical
+**  solution is used to update the vegetation cover.
+**
+**  Finally, the critical shear stress is updated at each node using:
+**    Tc = Tcb + V Tcv
+**  where Tcb is critical shear stress in the absence of cover (bare)
+**  and Tcv is critical shear stress under 100% cover.
+**
+**  Created January 2000, GT
+**
+\**************************************************************************/
+
+void tVegetation::UpdateVegetation( tMesh<tLNode> *meshPtr, double stormdur,
+                                    double interstormdur )
+{
+   tMeshListIter< tLNode > niter( meshPtr->getNodeList() ); // Node iterator
+   tLNode * cn;   // Ptr to current node
+   double tauex,  // Excess shear stress
+       veg;       // Fractional vegetation cover
+   
+   // Loop on active nodes, computing erosion from the previous storm
+   // and regrowth during the subsequent interstorm period.
+   // Key assumptions: we "cheat" by changing the critical shear stress only
+   // "after" the storm, using the initial value to calculate the
+   // subsequent, eroded value, rather than solving the full quadratic
+   // equation for vegetation destruction.
+   //   For both erosion and regrowth, we use an analytical solution for
+   // veg cover given its initial value and duration of erosion/regrowth.
+   for( cn=niter.FirstP(); niter.IsActive(); cn=niter.NextP() )
+   {
+      // Erosion of vegetation during storm (if any)
+      tauex = cn->getTau() - cn->getTauCrit();
+      veg = cn->getVegCover().getVeg();
+      if( tauex>0.0 )
+      {
+          veg = veg * exp( -mdKvd * tauex * stormdur );
+          cn->getVegCover().mdVeg = veg;
+          //cout << "veg after erosion: " << veg << endl;
+      }
+      
+      // Regrowth following storm
+      //cout << "veg before regrowth: " << veg << endl;
+      veg = 1.0 - (1.0 - veg) * exp( -interstormdur / mdTVeg );
+      cn->getVegCover().mdVeg = veg;
+      //cout << "veg after regrowth: " << veg << endl;
+
+      // Update critical shear stress
+      cn->setTauCrit( mdTauCritBare + veg*mdTauCritVeg );
+      //cout << "tau crit: " << mdTauCritBare + veg*mdTauCritVeg << endl;
+   }
+   
+}
+
+
+
---
+++ Child/Code/tVegetation/tVegetation.h
@@ -0,0 +1,78 @@
+/**************************************************************************\
+**
+**  tVegetation.h: Header file for the tVegetation and tVegCover classes
+**
+**  Classes tVegetation and tVegCover represents the vegetation cover
+**  across a terrain. Class tVegCover represents the properties of
+**  vegetation at a point on the landscape (e.g., the %cover), while
+**  class tVegetation represents the landscape-wide ("global")
+**  properties (e.g., regrowth coefficient). Class tVegCover is designed
+**  to be embedded in a node (in other words, each node "has a"
+**  vegetation cover).
+**
+**  Design notes:
+**  Classes tVegetation and tVegCover work together, with one being
+**  "local" (at-a-node) and the other "global" (common properties across
+**  the terrain and functions for terrain-wide updating). Class tVegCover
+**  holds tVegetation as a friend, so tVegetation methods can directly
+**  access its members. However, tVegCover provides no public "set"
+**  methods, so that only the routines in tVegetation can modify the
+**  vegetation properties.
+**
+**  Created January, 2000, GT
+**  
+**  $Id: tVegetation.h,v 1.1 2000-01-25 18:58:28 gtucker Exp $
+\**************************************************************************/
+
+#ifndef TVEGETATION_H
+#define TVEGETATION_H
+
+#include <math.h>
+#include "../tInputFile/tInputFile.h"
+//#include "../tMesh/tMesh.h"
+template<class tLNode> class tMesh;
+
+
+class tVegetation
+{
+  public:
+   tVegetation();
+   tVegetation( tMesh<class tLNode> *, tInputFile & );
+   void UpdateVegetation( tMesh<class tLNode> *, double, double );
+  private:
+   double mdKvd;   // Vegetation erosion coefficient (dim's LT/M)
+   double mdTVeg;  // Vegetation regrowth time scale (years)
+   double mdTauCritBare;  // Erosion threshold on bare soil
+   double mdTauCritVeg;   // Erosion threshold under 100% cover
+};
+
+class tVegCover
+{
+   friend class tVegetation;
+
+  public:
+   tVegCover();
+   tVegCover( const tVegCover & );
+   double getVeg();
+   
+  private:
+   double mdVeg;
+};
+
+inline tVegCover::tVegCover() 
+{
+   mdVeg = 1.0;
+}
+
+inline tVegCover::tVegCover( const tVegCover &orig ) 
+{
+   mdVeg = orig.mdVeg;
+}
+
+inline double tVegCover::getVeg() 
+{
+   return mdVeg;
+}
+
+
+#endif
