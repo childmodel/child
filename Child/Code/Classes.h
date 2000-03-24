098f95c7aac431daac0205d0f5f334db89d1d6e4
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2000-03-24 16:51:54 +0000

    Initial revision

---
+++ Child/Code/Classes.h
@@ -0,0 +1,44 @@
+// Classes used in CHILD model
+
+#ifndef CLASSES_H
+#define CLASSES_H
+
+/** CLASSES ********************************************************/
+class tTriangle;
+class tNode;
+class tLNode;
+class tEdge;
+template< class tSubNode > class tMesh;
+class tChannel;
+class tRegolith;
+class tErode;
+class tDeposit;
+class tMeander;
+class tBedrock;
+class tSurface;
+
+template< class T > class tArray;
+template< class NodeType > class tListNode;
+template< class NodeType > class tList;
+template< class NodeType > class tMeshList;
+template< class NodeType > class tPtrListNode;
+template< class NodeType > class tPtrList;
+template< class NodeType > class tListIter;
+template< class NodeType > class tMeshListIter;
+template< class NodeType > class tPtrListIter;
+
+class tInputFile;
+template< class tSubNode > class tListInputData;
+class tListIFStreams;
+template< class tSubNode > class tListOutputData;
+class tListOFStreams;
+
+class tEquilibCheck;
+class tSedTransPwrLaw;
+class tBedErodePwrLaw;
+class tStreamNet;
+class tStreamMeander;
+class tStorm;
+class tRunTimer;
+
+#endif
