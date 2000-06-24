93d355994a67a134d244c169d657327a2b3c8cae
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2000-06-24 15:15:18 +0000

    updated version #

--- Child/Code/Definitions.h
+++ Child/Code/Definitions.h
@@ -1,14 +1,14 @@
 /****************************************************************************\
 **   Definitions.h: Header file for defining global constants
 **
-**   $Id: Definitions.h,v 1.19 2000-06-01 14:30:19 gtucker Exp $
+**   $Id: Definitions.h,v 1.20 2000-06-24 15:15:18 gtucker Exp $
 \****************************************************************************/
 
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
 /** DEFINITIONS *************************************************************/
-#define VERSION "2.1.2, May 2000"
+#define VERSION "2.1.3, June 2000"
 #define TRUE 1
 #define FALSE 0
 #define kUniformMesh 0     /*method of grid construction*/
