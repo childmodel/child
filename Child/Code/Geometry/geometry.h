b660f5ab55f245eacdd0977225c858b6d8d1ce54
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2001-06-19 15:10:01 +0000

    Initial revision

---
+++ Child/Code/Geometry/geometry.h
@@ -0,0 +1,75 @@
+/***************************************************************************\
+**
+**  geometry.h
+**
+**  Definitions of some simple geometry classes.
+**
+**  $Id: geometry.h,v 1.1 2001-06-19 15:10:01 gtucker Exp $
+\***************************************************************************/
+
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+class Point2D
+{
+  public:
+   Point2D();
+   Point2D( double, double );
+   const Point2D &operator=( const Point2D & );
+   double x;
+   double y;
+};
+
+class Point3D
+{
+  public:
+   Point3D();
+   Point3D( double, double, double );
+   const Point3D &operator=( const Point3D & );
+   double x;
+   double y;
+   double z;
+};
+
+inline Point2D::Point2D() 
+{
+   x = y = 0.0;
+}
+
+
+inline Point2D::Point2D( double ix, double iy )
+{
+   x = ix;
+   y = iy;
+}
+
+inline const Point2D &Point2D::operator=(const Point2D &right )
+{
+    x = right.x;
+    y = right.y;
+    return *this;
+}
+
+inline Point3D::Point3D() 
+{
+   x = y = z = 0.0;
+}
+
+inline Point3D::Point3D( double ix, double iy, double iz )
+{
+   x = ix;
+   y = iy;
+   z = iz;
+}
+
+inline const Point3D &Point3D::operator=(const Point3D &right )
+{
+    x = right.x;
+    y = right.y;
+    z = right.z;
+    return *this;
+}
+
+
+
+#endif
