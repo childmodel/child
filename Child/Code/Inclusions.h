6e0b240fbbb3262d7378a75d6b5131b4a67702cd
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2002-04-10 14:38:01 +0000

    first commit of Inclusions.h

---
+++ Child/Code/Inclusions.h
@@ -0,0 +1,41 @@
+// Master list of included files for CHILD model
+
+#ifndef INCLUSIONS_H
+#define INCLUSIONS_H
+
+/** INCLUDED LIBRARY HEADER FILES **/
+//#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <iostream.h>
+#include <fstream.h>
+#include <string.h>
+#include <assert.h>
+
+/** INCLUDED FILES **********************************************************/
+#include "Definitions.h"
+#include "Classes.h"
+#include "errors/errors.h"
+#include "Mathutil/mathutil.h"
+#include "tArray/tArray.h"
+#include "tPtrList/tPtrList.h"
+#include "tList/tList.h"
+#include "tMeshList/tMeshList.h"
+#include "MeshElements/meshElements.h"
+#include "tInputFile/tInputFile.h"
+#include "tListInputData/tListInputData.h"
+//#include "tListOutputData/tListOutputData.h"
+//#include "tInputFile/tInputFile.h"
+#include "globalFns.h"
+#include "tLNode/tLNode.h"
+#include "tMesh/tMesh.h"
+#include "tStorm/tStorm.h"
+#include "tRunTimer/tRunTimer.h"
+#include "Erosion/erosion.h"
+#include "tStreamNet/tStreamNet.h"
+#include "tStreamMeander/tStreamMeander.h"
+#include "tOutput/tOutput.h"
+#include "tUplift/tUplift.h"
+//#include "tFault1/tFault1.h"   // commented out until tFault1 stable
+
+#endif
