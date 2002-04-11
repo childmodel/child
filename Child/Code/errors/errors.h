8358bdbb9026d694d72670c8870f6ac2fd078981
arnaud <arnaud@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2002-04-11 10:53:25 +0000

    added __attribute__ ((noreturn)) so that g++ knows that there is no return possible.

--- Child/Code/errors/errors.h
+++ Child/Code/errors/errors.h
@@ -4,7 +4,7 @@
 **  Header file for CHILD error-handling routines.
 **
 **  Created Dec. 97
-**  $Id: errors.h,v 1.4 2002-04-10 15:13:41 gtucker Exp $
+**  $Id: errors.h,v 1.5 2002-04-11 10:53:25 arnaud Exp $
 \****************************************************************/
 
 #ifndef ERRORS_H
@@ -13,6 +13,10 @@
 #include <stdlib.h>
 #include <iostream.h>
 
-void ReportFatalError( const char *errStr );
+void ReportFatalError( const char *errStr )
+#ifdef __GNUC__
+ __attribute__ ((noreturn))
+#endif
+;
 
 #endif
