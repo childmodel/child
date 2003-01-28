a914096e21e6469b6303f9b0bb389ad447c311a5
childcvs <childcvs@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2003-01-28 13:56:34 +0000

    Arnaud: the Intel compiler understands __attribute__((noreturn)). Let' use it.

--- Child/Code/errors/errors.h
+++ Child/Code/errors/errors.h
@@ -6,17 +6,21 @@
 **  @brief Header file for CHILD error-handling routines.
 **
 **  Created Dec. 97
-**  $Id: errors.h,v 1.8 2003-01-17 17:30:26 childcvs Exp $
+**  $Id: errors.h,v 1.9 2003-01-28 13:56:34 childcvs Exp $
 */
 /****************************************************************/
 
 #ifndef ERRORS_H
 #define ERRORS_H
 
-void ReportFatalError( const char *errStr )
-#ifdef __GNUC__
- __attribute__ ((noreturn))
+#ifndef ATTRIBUTE_NORETURN
+# if defined(__GNUC__) || defined(__INTEL_COMPILER)
+#  define ATTRIBUTE_NORETURN __attribute__ ((noreturn))
+# else
+#  define ATTRIBUTE_NORETURN
+# endif
 #endif
-;
+
+void ReportFatalError( const char *errStr ) ATTRIBUTE_NORETURN;
 
 #endif
