a79b87a883d74810faf80a24d9dc10e371bd7ac5
childcvs <childcvs@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2003-08-06 16:12:22 +0000

    (Arnaud) use compiler.h

--- Child/Code/errors/errors.h
+++ Child/Code/errors/errors.h
@@ -6,20 +6,14 @@
 **  @brief Header file for CHILD error-handling routines.
 **
 **  Created Dec. 97
-**  $Id: errors.h,v 1.9 2003-01-28 13:56:34 childcvs Exp $
+**  $Id: errors.h,v 1.10 2003-08-06 16:12:22 childcvs Exp $
 */
 /****************************************************************/
 
 #ifndef ERRORS_H
 #define ERRORS_H
 
-#ifndef ATTRIBUTE_NORETURN
-# if defined(__GNUC__) || defined(__INTEL_COMPILER)
-#  define ATTRIBUTE_NORETURN __attribute__ ((noreturn))
-# else
-#  define ATTRIBUTE_NORETURN
-# endif
-#endif
+#include "../compiler.h"
 
 void ReportFatalError( const char *errStr ) ATTRIBUTE_NORETURN;
 
