26389983cffb8d7fc3fd71c4aaa3912104f37197
gtucker <gtucker@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2011-08-03 20:45:16 +0000

    Added some new (and old) makefiles to the Bin folder. childsharedlib.mk and gcclib.mk are for compiling CHILD as a shared library: one step in componentizing or python-module-izing. GT Aug '11

---
+++ Child/Bin/gccmac.mk
@@ -0,0 +1,33 @@
+# GNU compiler (mac OS X)
+
+CXX = g++
+#
+# -O is necessary for -Wuninitialized to be on
+# -Weffc++ -fmessage-length=0 gives useful but noisy warnings
+WARNINGFLAGS = -pedantic -Wall -W \
+	-Wwrite-strings \
+	-Wpointer-arith -Wcast-qual -Wcast-align
+
+# recent versions of Linux use new style casts in their headers
+WARNINGFLAGS += -Wold-style-cast
+
+# gcc 2.x does not put the standard C++ headers in the namespace "std"
+# In such a case, remove the comment of the following line.
+#WARNINGFLAGS += -DHAVE_NO_NAMESPACE
+
+# -mpowerpc64 etc: generates code for 64 bit G5 (power pc 970)
+# will not run on G3, G4, etc.
+#ARCH := -mcpu=G5 -mtune=G5 -mpowerpc64 -mpowerpc-gpopt
+ARCH :=
+# optimise
+CFLAGS = $(WARNINGFLAGS) -g -O2 $(ARCH) -c
+LDFLAGS = $(WARNINGFLAGS) -g -O2 $(ARCH)
+# no optimisation, build is faster
+#CFLAGS = $(WARNINGFLAGS) -g $(ARCH) -c
+#LDFLAGS = $(WARNINGFLAGS) -g $(ARCH)
+LIBS =
+
+LDFLAGS += -o $@
+
+OBJEXT = o
+EXEEXT =
