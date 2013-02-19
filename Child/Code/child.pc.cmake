832be242af8d0a277a96cc87b5264891975d7204
huttone <huttone@ffd40f59-54b9-4234-bcd0-80a5451590cd>
2013-02-19 18:45:12 +0000

    * Add extra -I flags to specify paths to child header files.

--- Child/Code/child.pc.cmake
+++ Child/Code/child.pc.cmake
@@ -2,5 +2,5 @@ Name: child
 Description: Child library
 Version: ${CHILD_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lchild
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include/
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include -I${CMAKE_INSTALL_PREFIX}/include/child -I${CMAKE_INSTALL_PREFIX}/include/child/ChildInterface
 
