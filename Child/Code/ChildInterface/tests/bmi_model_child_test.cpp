1db69cf3af248a0aa24bfa1dc356862e7ae40f18
mcflugen <mcflugen@gmail.com>
2016-11-07 10:35:19 -0700

    Use new BMI implementation.

--- Child/Code/ChildInterface/tests/bmi_model_child_test.cpp
+++ Child/Code/ChildInterface/tests/bmi_model_child_test.cpp
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "../bmi_model_child.h"
+#include "../bmi_model.h"
+// #include "../bmi_model_child.h"
 
 #define CHECK_INTERFACE_FUNC(f_name, call) { \
   fprintf (stdout, "\033[32m%s... \033[39m", f_name); \
@@ -46,7 +47,7 @@ main (int argc, char *argv[])
 
   { /* Model information functions */
     int i;
-    char model_name[bmi::COMPONENT_NAME_MAX];
+    char model_name[bmi::MAX_COMPONENT_NAME];
     int input_var_count;
     int output_var_count;
     char **input_names = NULL;
@@ -69,22 +70,23 @@ main (int argc, char *argv[])
 
     input_names = (char**) malloc (sizeof (char*) * input_var_count);
     for (i=0; i<input_var_count; i++)
-      input_names[i] = (char*) malloc (sizeof (char) * bmi::VAR_NAME_MAX);
+      input_names[i] = (char*) malloc (sizeof (char) * bmi::MAX_VAR_NAME);
 
     CHECK_INTERFACE_FUNC ("Get_input_var_names", child.GetInputVarNames (input_names));
 
     output_names = (char**) malloc (sizeof (char*) * output_var_count);
     for (i=0; i<output_var_count; i++)
-      output_names[i] = (char*) malloc (sizeof (char) * bmi::VAR_NAME_MAX);
+      output_names[i] = (char*) malloc (sizeof (char) * bmi::MAX_VAR_NAME);
 
     CHECK_INTERFACE_FUNC ("Get_output_var_names", child.GetOutputVarNames (output_names));
 
     for (i=0, name=input_names; i<input_var_count; i++, name++) {
-      bmi::VarType var_type;
-      char units[bmi::UNITS_NAME_MAX];
+      char var_type[bmi::MAX_UNITS_NAME];
+      char units[bmi::MAX_UNITS_NAME];
+      char grid_type[bmi::MAX_UNITS_NAME];
       int rank;
+      int grid;
       int number_of_points;
-      bmi::GridType grid_type;
       double *buffer = NULL;
       int *shape;
       double *spacing;
@@ -95,10 +97,12 @@ main (int argc, char *argv[])
       fprintf (stdout, "==================================\n");
 
       /* Variable information functions */
-      CHECK_INTERFACE_FUNC ("Get_var_type", var_type = child.GetVarType (*name));
-      CHECK_INTERFACE_FUNC ("Get_var_rank", rank = child.GetVarRank (*name));
+      CHECK_INTERFACE_FUNC ("Get_var_type", child.GetVarType (*name, var_type));
+      CHECK_INTERFACE_FUNC ("GetVarGrid", grid = child.GetVarGrid (*name));
+      CHECK_INTERFACE_FUNC ("Get_var_rank", rank = child.GetGridRank (grid));
 
-      CHECK_INTERFACE_FUNC ("Get_var_point_count", number_of_points = child.GetVarPointCount (*name));
+      // CHECK_INTERFACE_FUNC ("Get_var_point_count", number_of_points = child.GetVarPointCount (*name));
+      CHECK_INTERFACE_FUNC ("GetGridSize", number_of_points = child.GetGridSize (grid));
 
       buffer = (double*) malloc (sizeof (double) * number_of_points);
 
@@ -106,16 +110,17 @@ main (int argc, char *argv[])
       //CHECK_INTERFACE_FUNC ("bmi_Get_double", bmi_Get_double (child, *name, buffer));
 
       /* Variable setters */
-      CHECK_INTERFACE_FUNC ("Set_double", child.SetDouble (*name, buffer));
+      CHECK_INTERFACE_FUNC ("SetValue", child.SetValue (*name, (void*)buffer));
       free (buffer);
     }
 
     for (i=0, name=output_names; i<output_var_count; i++, name++) {
-      bmi::VarType var_type;
-      char units[bmi::UNITS_NAME_MAX];
+      char var_type[bmi::MAX_UNITS_NAME];
+      char units[bmi::MAX_UNITS_NAME];
+      char grid_type[bmi::MAX_UNITS_NAME];
       int rank;
+      int grid;
       int number_of_points;
-      bmi::GridType grid_type;
       double *buffer = NULL;
       int *shape;
       double *spacing;
@@ -126,21 +131,23 @@ main (int argc, char *argv[])
       fprintf (stdout, "==================================\n");
 
       /* Variable information functions */
-      var_type = child.GetVarType (*name);
-      rank = child.GetVarRank (*name);
-      number_of_points = child.GetVarPointCount (*name);
+      child.GetVarType (*name, var_type);
+      grid = child.GetVarGrid (*name);
+      rank = child.GetGridRank (grid);
+      number_of_points = child.GetGridSize (grid);
 
-      fprintf (stdout, "            Type: %d\n", var_type);
+      fprintf (stdout, "            Type: %s\n", var_type);
       fprintf (stdout, "            Rank: %d\n", rank);
       fprintf (stdout, "Number of points: %d\n", number_of_points);
     }
 
     for (i=0, name=output_names; i<output_var_count; i++, name++) {
-      bmi::VarType var_type;
-      char units[bmi::UNITS_NAME_MAX];
+      char var_type[bmi::MAX_UNITS_NAME];
+      char units[bmi::MAX_UNITS_NAME];
+      char grid_type[bmi::MAX_UNITS_NAME];
       int rank;
+      int grid;
       int number_of_points;
-      bmi::GridType grid_type;
       int *shape;
       double *spacing;
       double *origin;
@@ -150,23 +157,24 @@ main (int argc, char *argv[])
       fprintf (stdout, "==================================\n");
 
       /* Variable information functions */
-      CHECK_INTERFACE_FUNC ("Get_var_type", var_type = child.GetVarType (*name));
-      CHECK_INTERFACE_FUNC ("Get_var_rank", rank = child.GetVarRank (*name));
+      CHECK_INTERFACE_FUNC ("Get_var_type", child.GetVarType (*name, var_type));
+      CHECK_INTERFACE_FUNC ("Get_var_grid", grid = child.GetVarGrid (*name));
+      CHECK_INTERFACE_FUNC ("Get_var_rank", rank = child.GetGridRank (grid));
 
-      CHECK_INTERFACE_FUNC ("Get_var_point_count", number_of_points = child.GetVarPointCount (*name));
+      CHECK_INTERFACE_FUNC ("GetGridSize", number_of_points = child.GetGridSize (grid));
 
       if (number_of_points > 0) {
         double *buffer = (double*) malloc (sizeof (double) * number_of_points);
 
         /* Variable getters */
-        CHECK_INTERFACE_FUNC ("Get_double", child.GetDouble (*name, buffer));
+        CHECK_INTERFACE_FUNC ("GetValue", child.GetValue (*name, (void*)buffer));
 
         /* Variable setters */
         //CHECK_INTERFACE_FUNC ("bmi_Set_double", bmi_Set_double (child, *name, buffer));
         free (buffer);
 
-        CHECK_INTERFACE_FUNC ("Get_double_ptr", buffer = child.GetDoublePtr (*name));
-
+        // CHECK_INTERFACE_FUNC ("Get_double_ptr", buffer = child.GetDoublePtr (*name));
+/*
         try {
           buffer = child.GetDoublePtr (*name);
           if (rank > 0) {
@@ -176,42 +184,7 @@ main (int argc, char *argv[])
           }
         }
         catch (bmi::NonFatalError) { }
-
-        if (rank > 0) {
-          CHECK_INTERFACE_FUNC ("Get_grid_type", grid_type = child.GetGridType (*name));
-
-          if (grid_type == bmi::GRID_TYPE_UNIFORM) {
-            shape = (int*) malloc (sizeof (int) * rank);
-            spacing = (double*) malloc (sizeof (double) * rank);
-            origin = (double*) malloc (sizeof (double) * rank);
-
-            CHECK_INTERFACE_FUNC ("Get_grid_shape", child.GetGridShape (*name, shape));
-            CHECK_INTERFACE_FUNC ("Get_grid_spacing", child.GetGridSpacing (*name, spacing));
-            CHECK_INTERFACE_FUNC ("Get_grid_origin", child.GetGridOrigin (*name, origin));
-
-            free (origin);
-            free (spacing);
-            free (shape);
-          }
-          else if (grid_type == bmi::GRID_TYPE_UNSTRUCTURED) {
-            CHECK_INTERFACE_FUNC ("GetVarVertexCount", child.GetVarVertexCount (*name));
-            CHECK_INTERFACE_FUNC ("GetVarCellCount", child.GetVarCellCount (*name));
-
-            const int number_of_vertices = child.GetVarVertexCount (*name);
-            const int number_of_cells = child.GetVarCellCount (*name);
-            double * x = new double[number_of_points];
-            double * y = new double[number_of_points];
-            int * c = new int[number_of_vertices];
-            int * o = new int[number_of_cells];
-
-            CHECK_INTERFACE_FUNC ("GetGridX", child.GetGridX (*name, x));
-            CHECK_INTERFACE_FUNC ("GetGridY", child.GetGridX (*name, y));
-            CHECK_INTERFACE_FUNC ("GetGridConnectivity", child.GetGridConnectivity (*name, c));
-            CHECK_INTERFACE_FUNC ("GetGridOffset", child.GetGridOffset (*name, o));
-
-            delete x, y, c, o;
-          }
-        }
+*/
       }
     }
   }
