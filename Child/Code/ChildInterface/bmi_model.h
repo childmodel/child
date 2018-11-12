10e0725a5df6ccdd495de3bffb713d32514fce37
mcflugen <mcflugen@gmail.com>
2018-11-12 16:05:44 -0700

    Implement Bmi virtual class; embed Child class.

--- Child/Code/ChildInterface/bmi_model.h
+++ Child/Code/ChildInterface/bmi_model.h
@@ -1,31 +1,11 @@
 #ifndef CHILD_H_
 #define CHILD_H_
 
-#define BMI_GET_GRID_CONNECTIVITY
-
+#include "bmi.h"
 #include "child.h"
 
 
-namespace bmi {
-
-const int MAX_COMPONENT_NAME = 2048;
-const int MAX_VAR_NAME = 2048;
-const int MAX_UNITS_NAME = 2048;
-
-typedef enum {
-  FAILURE = 1,
-  BAD_ARGUMENT,
-  BAD_VAR_NAME,
-  BAD_FILE,
-  CLASS_NOT_INITIALIZED,
-} FatalError;
-
-typedef enum {
-  FUNCTION_NOT_IMPLEMENTED = -1,
-} NonFatalError;
-
-
-class Model : public Child {
+class Model : public bmi::Bmi {
  public:
   Model ();
 
@@ -58,12 +38,12 @@ class Model : public Child {
 
   // Variable getters
   void GetValue (const char * var_name, void *);
-  double * GetValuePtr (const char * var_name) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
-  void GetValueStride (const char * var_name, int * const stride) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
+  // double * GetValuePtr (const char * var_name) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
+  // void GetValueStride (const char * var_name, int * const stride) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
 
   // Variable setters
   void SetValue (const char *, void *);
@@ -73,47 +53,43 @@ class Model : public Child {
   int GetGridRank (const int grid_id);
   int GetGridSize (const int grid_id);
 
-  void GetGridShape (const int, int *) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
-  void GetGridSpacing (const int, double *) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
-  void GetGridOrigin (const int, double *) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
+  // void GetGridShape (const int, int *) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
+  // void GetGridSpacing (const int, double *) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
+  // void GetGridOrigin (const int, double *) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
 
   void GetGridX (const int, double * const);
   void GetGridY (const int, double * const);
-  void GetGridZ (const int, double * const) {
-    throw bmi::FUNCTION_NOT_IMPLEMENTED;
-  }
+  // void GetGridZ (const int, double * const) {
+  //   throw bmi::FUNCTION_NOT_IMPLEMENTED;
+  // }
 
   int GetGridFaceCount(const int);
   int GetGridVertexCount(const int);
   void GetGridConnectivity (const int, int * const );
   void GetGridOffset (const int, int * const);
 
-  int GetGridNumberOfEdges(const int);
+  int GetGridNumberOfEdges(const int) { return -1; }
   int GetGridNumberOfFaces(const int);
 
-  void GetGridEdgeNodes(const int, int * const);
-  void GetGridFaceEdges(const int, int * const);
+  void GetGridEdgeNodes(const int, int * const) { }
+  void GetGridFaceEdges(const int, int * const) { }
   void GetGridFaceNodes(const int, int * const);
   void GetGridNodesPerFace(const int, int * const);
 
-
- private:
-  bool HasInputVar (const char * var_name);
-  bool HasOutputVar (const char * var_name);
-
+private:
   int input_var_name_count;
   int output_var_name_count;
 
-  char input_var_names[5][2048];
-  char output_var_names[6][2048];
-};
+  char **input_var_names;
+  char **output_var_names;
 
-} // namespace bmi
+  Child model;
+};
 
 #endif
